af287f2aaf38b3a9ee0b75ae6dd97a7abfb98556
Salman Alsuwailem <salman6111@gmail.com>
2020-02-26 16:13:43 +0300

    Updates

---
+++ precache-manifest.b2eeb5b77e681ff654ea0eaebc2ce14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4e9307b007a6f614aff0f17a238f72",
+    "url": "/Marketplace-POS-Project2/index.html"
+  },
+  {
+    "revision": "a81e95038e0df8e4c107",
+    "url": "/Marketplace-POS-Project2/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2c24edc642d1644ef1d1",
+    "url": "/Marketplace-POS-Project2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a81e95038e0df8e4c107",
+    "url": "/Marketplace-POS-Project2/static/js/2.c44ea21b.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Marketplace-POS-Project2/static/js/2.c44ea21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c24edc642d1644ef1d1",
+    "url": "/Marketplace-POS-Project2/static/js/main.fdeb491a.chunk.js"
+  },
+  {
+    "revision": "a23472f3c322a58bc7f2",
+    "url": "/Marketplace-POS-Project2/static/js/runtime-main.5f881bf9.js"
+  }
+]);
